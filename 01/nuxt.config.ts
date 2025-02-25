5ba5cabee1735dc322d555553ab42f472290c15d
ARAKI Takeo <araki.takeo@robon.co.jp>
2025-02-25 06:31:59 +0000

    eslint の適用

--- 01/nuxt.config.ts
+++ 01/nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: ['@nuxt/eslint', 'vuetify-nuxt-module'],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-11-01',
   eslint: {
     config: {
-      stylistic: true
-    }
+      stylistic: true,
+    },
   },
   vuetify: {
     moduleOptions: {},
-    vuetifyOptions: {}
-  }
+    vuetifyOptions: {},
+  },
 })
